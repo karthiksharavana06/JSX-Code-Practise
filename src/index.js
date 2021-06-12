b706fe4b7164044ab3039cc96611d70a4db86546
Karthikeyan Sharavanakkumar <46404198+karthiksharavana06@users.noreply.github.com>
2021-06-12 14:18:57 +0530

    code practice for react with list items

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My fav Food</h1>
+    <ul>
+      <li>Biriyani</li>
+      <li>Shawarma</li>
+      <li>Grill</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
