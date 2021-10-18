afb27954bc94f33e943043647b29ae95b015d11c
Karthikeyan Sharavanakkumar <46404198+karthiksharavana06@users.noreply.github.com>
2021-10-18 12:48:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
 import React from "react";
 import ReactDOM from "react-dom";
 
